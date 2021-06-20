0c798e558b021f40fd89656e72c708246a771282
mdeamf <mauriciodealmeidamf@gmail.com>
2021-06-20 18:21:32 -0300

    feat: adding button to home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import ListItemsComponent from './components/ListItemsComponent';
+import Button from './components/ButtonComponent';
 
 function App() {
   return (
     <div className="App">
       <p>Learn React</p>
       <ListItemsComponent />
+      <Button title="Snapshot Testing" />
     </div>
   );
 }
